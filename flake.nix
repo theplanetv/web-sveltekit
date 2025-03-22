06d9492d1c56c150750bbaad9177223f2910a5a7
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-03-22 10:10:50 +0700

    Add nix flake devshell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "web-sveltekit flake for theplanetv website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+  in {
+    devShells = forAllSystems (system:
+      let
+        pkgs = nixpkgsFor.${system};
+      in {
+        default = with pkgs;
+          mkShell {
+            nativeBuildInputs = [
+              deno
+            ];
+          };
+      });
+  };
+}
